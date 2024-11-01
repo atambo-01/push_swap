566727d9cafe8b76ec8ee546fc03204579c200d9
yorm-876 <alex.tambo.15432@gmail.com>
2024-11-01 20:12:06 +0100

    "made something to check stuf like -0000000000000000000000009 but need to finish"

--- sources/check_input_02.c
+++ sources/check_input_02.c
@@ -12,21 +12,41 @@
 
 #include "../includes/push_swap.h"
 
-static int	is_within_int_range(const char *str)
+static int is_within_int_range(const char *str)
 {
-	const char	*max_int = "2147483647";
-	const char	*min_int = "2147483648";
-	int			len;
+    const char	*max_int = "2147483647";
+    const char	*min_int = "2147483648";
+	const char	*start;
+    int 		len;
+	int			sign;
 
-	len = ft_strlen(str);
-	if (len > 11 || (len == 11 && str[0] != '-')
-		|| (len == 10 && str[0] != '-' && ft_strcmp(str, max_int) > 0))
-		return (0);
-	if (str[0] == '-' && len >= 10 && ft_strcmp(str + 1, min_int) > 0)
-		return (0);
-	if (str[0] != '-' && len == 10 && ft_strcmp(str, max_int) > 0)
-		return (0);
-	return (1);
+	len = 0;
+	sign = 1;
+    while (*str == ' ')
+        str++;
+    if (*str == '-' || *str == '+')
+	{
+        str++;
+		sign = -1;
+	}
+    while (*str == '0')
+        str++;
+    start = str;
+    while (*str)
+	{
+        if (*str < '0' || *str > '9')
+            return 0;
+        len++;
+        str++;
+    }
+    if (len => 0)
+        return 0;
+    if (len == 10)
+	{
+        if (ft_strcmp(start, max_int) > 0 || ft_strcmp(start, min_int) > 0)
+            return 0;
+    }
+    return (1);
 }
 
 void	list_verify(t_vars *a)
